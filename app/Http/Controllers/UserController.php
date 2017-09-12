49c0f4eb8d227963f499424c18b8ef8d629e9ddf
Rasel Ahmed <raselahmed1612@gmail.com>
2017-09-12 23:28:08 +0600

    Validation Complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,14 @@ class UserController extends Controller
 
 	public function postRegistration(Request $request)
 	{
+		$this->validate($request, [
+			'first_name' => 'required|max:80',
+			'email' => 'required|email|unique:users',
+			'std_id' => 'required|min:8',
+			'session' => 'required|min:7',
+			'password' => 'required|min:6'
+		]);
+
 		$first_name = $request['first_name'];
 		$last_name = $request['last_name'];
 		$email = $request['email'];
@@ -29,11 +37,18 @@ class UserController extends Controller
 
 		$user->save();
 
+		Auth::login($user);
+
 		return redirect()->route('dashboard');
 	}
 
 	public function postLogin(Request $request)
 	{
+		$this->validate($request, [
+			'email' => 'required',
+			'password' => 'required'
+		]);
+
 		if (Auth::attempt(['email'=>$request['email'], 'password'=>$request['password']])){
 			return redirect()->route('dashboard');
 		}
